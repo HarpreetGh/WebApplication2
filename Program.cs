4211bab883dbf9d8225de9bfdbfd4f1fc114d8c1
Harpreet Ghag <70241088+HarpreetGh@users.noreply.github.com>
2025-01-23 11:30:23 -0800

    Default Middleware

--- Program.cs
+++ Program.cs
@@ -1,54 +1,38 @@
-using System.Text;
-using System.Text.Json;
-
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+builder.Services.AddHttpLogging(logging => {
+    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+});
+
 var app = builder.Build();
 
-Blog defaultBlog = new Blog("First Blog", "I am happy!");
-List<Blog> blogs= [defaultBlog];
+if (app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/error");
+}
+else { app.UseDeveloperExceptionPage(); }
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpLogging();
+
+app.Use(async (context, next) => {
+    Console.WriteLine($"Path: {context.Request.Path}");
+    await next.Invoke();
+    Console.WriteLine($"Response Status Code: {context.Response.StatusCode}");
+});
 
 app.Use(async (context, next) => {
     var startTime = DateTime.Now;
+    Console.WriteLine($"Start Time: {startTime}");
     await next.Invoke();
-    Console.WriteLine($"Path Called: {context.Request.Path} in Duration: {DateTime.Now - startTime}");
+    var duration = DateTime.Now - startTime;
+    Console.WriteLine($"Response Time: {duration.TotalMilliseconds} ms");
 });
-
-app.UseWhen(context => context.Request.Path == "/reset",
-    app => app.Use(async (context, next) => {
-    if (context.Request.Headers["pass"] == "P@ssword!") {
-        await next.Invoke();
-    }
-    else {
-        context.Response.StatusCode = 403;
-        await context.Response.WriteAsync("Access Denied");
-        Console.WriteLine("Access Denied");
-    }
-}));
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/blogs", () => blogs);
+app.MapGet("/test", () => { throw new Exception("Test"); });
+app.MapGet("/error", () => "Oopsie an error occured.");
 
-app.MapPost("/newBlog/{title:minlength(3)}", (string title, string? body) => { blogs.Add(new Blog(title, body)); return Results.Ok(blogs.Last()); });
-
-app.MapPost("/newBlog/", (Blog b) => { blogs.Add(b); return Results.Ok(b); });
-
-app.MapPost("/newBlogHash/", (string bStr) => {
-    // not tested but idea
-    string hash = Convert.ToBase64String(Encoding.UTF8.GetBytes(bStr));
-    if(hash != hash) {
-        return Results.Problem("Hash doesn't match");
-    }
-    Blog b = JsonSerializer.Deserialize<Blog>(bStr);
-    blogs.Add(b);
-    return Results.Ok(b);
-});
-
-app.MapPost("/reset", () => { blogs.Clear(); blogs = [defaultBlog]; return Results.Accepted("RESET!"); });
 
 app.Run();
-
-public class Blog(string title, string body)
-{
-    public string Title { get; set; } = title;
-    public string Body { get; set; } = body;
-}
--- obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c6c02786bd752af42f36c416cfb1da5a68a68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9661276329ef8348d6c18054270cc1047971b405")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
